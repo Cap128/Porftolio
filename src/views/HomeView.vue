ea419a7a6af7d6113f9f876a928403ca4ef35676
T_c <miguelito.vargasf@gmail.com>
2024-09-04 09:53:32 -0500

    init portfolio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="App">
+      <header class="App-header">
+        <div class="header-left">
+          <div class="header-row">Fila 1</div>
+          <div class="header-row">Fila 2</div>
+          <div class="header-row">Fila 3</div>
+        </div>
+        <div class="header-right">
+          <!-- <img src="tu-imagen.png" alt="Imagen" className="header-image" /> -->
+        </div>
+      </header>
+    </div>
   </main>
 </template>
